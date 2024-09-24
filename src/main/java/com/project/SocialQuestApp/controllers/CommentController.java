718e5c8ab7922571daffa2d481cd6a1a03b9d436
murat <mrat.sag@hotmail.com>
2024-09-24 15:54:42 +0300

    fixed error

--- src/main/java/com/project/SocialQuestApp/controllers/CommentController.java
+++ src/main/java/com/project/SocialQuestApp/controllers/CommentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/comment")
+@RequestMapping("/comments")
 public class CommentController {
     private CommentService commentService;
 
